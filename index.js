77c3a978b8839bc5723e00aecff12443b7af2909
Neon Ratemo <neon.ratemo@student.moringaschool.com>
2023-07-04 22:20:16 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName;
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'new best customer';
+}
+
+const leastFavoriteCustomer = 'Alice';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Bob';
+}
